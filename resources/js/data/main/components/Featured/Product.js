a6f98ac995d171d99500d22ad3b5249a88d4b373
M Aslam <aslam4webz@gmail.com>
2020-10-17 17:17:09 +0300

    image error solved 2

--- resources/js/data/main/components/Featured/Product.js
+++ resources/js/data/main/components/Featured/Product.js
@@ -1,8 +1,6 @@
 
 import React, { useEffect, useState } from 'react'
 import styled from 'styled-components'
-import Axios from 'axios'
-import { Link } from 'react-router-dom'
 
 const Div = styled.div`
 .card{
